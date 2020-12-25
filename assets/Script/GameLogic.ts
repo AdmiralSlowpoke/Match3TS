e6547bbaff2f87baf2ea5e10b6a9b9ddc8c8e04e
AdmiralSlowpoke <maksimzlt@gmail.com>
2020-12-25 19:20:36 +0500

    Новый вариант проверки

--- assets/Script/GameLogic.ts
+++ assets/Script/GameLogic.ts
@@ -26,23 +26,44 @@ export default class GameLogic extends cc.Component {
     }
     GenerateLayout(){
         let field:number[][]=[];
-        //Генерируем числовое поле
-        for(let i=0;i<9;i++){
+        //Генерируем числовое поле 7x8
+        for(let i=0;i<11;i++){
             field[i]=[];
-            for(let j=0;j<10;j++){
-                if(i==0||j==0||i==8||j==9)
+            for(let j=0;j<12;j++){
+                if(i<2||j<2||i>8||j>9)
                 field[i][j]=9;
                 else
                 field[i][j]=RandomInt(0,4);
             }
         }
+        
         //Пока что без проверки на начальные комбинации
         //Потом не допускать чтобы в ряду и строке было более двух одинаковых чисел
         //А так же более 3 одинаковых элементов в одном квадрате 2 на 2
+        for(let i=2;i<9;i++){
+            cc.log(field[i]);
+            for(let j=2;j<10;j++){
+                //Горизонтальная проверка
+                if(field[i][j]==field[i][j+1]&&field[i][j]==field[i][j+2])
+                    field[i][j+2]=RandomExcept([field[i-1][j+2],field[i][j]]);
+                //Вертикальная проверка
+                if(field[i][j]==field[i+1][j]&&field[i][j]==field[i+2][j])
+                    field[i+2][j]=RandomExcept([field[i+2][j-1],field[i][j]]);
+                //Проверка квадратом
+                if(field[i][j]==field[i+1][j]&&field[i][j]==field[i][j+1]&&field[i][j]==field[i+1][j+1])
+                    field[i+1][j+1]==RandomExcept([field[i][j],field[i+1][j+2]]);
 
-
+                let node=cc.instantiate(this.BlankCube);
+                node.getComponent(cc.Sprite).spriteFrame=this.Cubes[field[i][j]];
+                node.parent=this.GridLayout;
+            }
+        }
+        /*
+        
         //Горизонтальная проверка
+        cc.log("Горизонтальная");
         for(let i=1;i<8;i++){
+            cc.log(field[i]);
             for(let j=1;j<7;j++){
                 if(field[i][j]==field[i][j+1]&&field[i][j]==field[i][j+2])
                 field[i][j+2]=RandomExcept([field[i][j+2],field[i+1][j+2],field[i-1][j+2],field[i][j+3]]);
@@ -50,20 +71,14 @@ export default class GameLogic extends cc.Component {
             }
         }
         //Вертикальная проверка
+        cc.log("Вертикальная");
         for(let i=1;i<6;i++){
+            cc.log(field[i]);
             for(let j=1;j<9;j++){
                 if(field[i][j]==field[i+1][j]||field[i][j]==field[i+2][j])
                 field[i+2][j]=RandomExcept([field[i+2][j],field[i+2][j+1],field[i+2][j-1],field[i+3][j]]);
             }
-        }
-        for(let i=1;i<8;i++){
-            cc.log(field[i]);
-            for(let j=1;j<9;j++){
-                let node=cc.instantiate(this.BlankCube);
-                node.getComponent(cc.Sprite).spriteFrame=this.Cubes[field[i][j]];
-                node.parent=this.GridLayout;
-            }
-        }
+        }*/
         
 }
 
@@ -71,7 +86,6 @@ export default class GameLogic extends cc.Component {
 function RandomExcept(mas:number[]){
     let a;
     let check;
-    cc.log("TEST");
     for(;;){
         a=RandomInt(0,4);
         check=false;
